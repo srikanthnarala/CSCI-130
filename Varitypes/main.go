93fb9456ac6b19e28bd64549adb36cd5ba1cba0b
srikanthnarala <srikanthnarala@mail.fresnostate.edu>
2016-02-01 01:48:07 -0800

    add

---
+++ Varitypes/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	Int := 57
+	Float := 9.7
+	String := "Srikanth"
+	Bool := true
+
+	fmt.Printf("%T\n", Int)
+	fmt.Printf("%T\n", Bool)
+	fmt.Printf("%T\n", String)
+	fmt.Printf("%T\n", Float)
+
+}
