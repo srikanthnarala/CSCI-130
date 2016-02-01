f387fce7eb028cdf0872abd7a77bcfaae6922ea6
srikanthnarala <srikanthnarala@mail.fresnostate.edu>
2016-02-01 05:54:21 -0800

    adds

---
+++ Sum-Square/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var total = 0	//total is sum of squares from 1 to 10 numbers
+	var total1=0	//total1 is square of sum of 1 to 10 numbers
+	var x int
+
+	for i := 1; i <= 10; i++ {
+		fmt.Scan(i)
+		total = total + (i * i)
+	}
+	fmt.Println("sum of squares from 1 to 10 is", total)
+	total1=0
+	for i:=1;i<=10 ;i++{
+		fmt.Scan(i)
+		total1=total1+i
+	}
+	total1=(total1*total1)
+	x=total1-total
+	fmt.Print("square of sum of 10 numbers is",total1)
+	fmt.Println("\n difference of square of sum and sum of squares from 1 to 10 numbers is",x)
+
+
+}
